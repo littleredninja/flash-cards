4bdb677a654931be4b9f890360d7700bb6ae1317
MarekT <rkx1901@gmail.com>
2014-08-09 15:00:39 -0500

    Added seeding file

--- db/migrate/20140808153839_create_cards.rb
+++ db/migrate/20140808153839_create_cards.rb
@@ -3,7 +3,7 @@ class CreateCards < ActiveRecord::Migration
 	def change
 		create_table :cards do |t|
 			t.string :question
-            t.string :answer
+      t.string :answer
 			t.integer :deck_id
 
 			t.timestamps
